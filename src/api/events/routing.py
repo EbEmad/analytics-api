b8b57b4270b0afd8abe4a38b450b53e258ed2f94
Ebrahim Emad <ebemad8@gmail.com>
2025-04-22 14:21:35 +0200

    adiing new features

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -25,21 +25,20 @@ def read_events()-> EventlistSchema:
 @router.post("/")
 def create_events(payload:EventCreateSchema)-> EventSchema:
     # a bunch of items in a table
-    print(payload)
+    print(payload.page)
     print(type(payload))
-    return {'id':5555}
+    data=payload.model_dump() # payload -> dict -> pydantic
+    return  {'id':123,**data}
 
 # update this data
 # PUT /api/events/id
 @router.put("/{event_id}")
-def update_events(event_id:int,payload:EventUpdateSchema)-> EventlistSchema:
+def update_events(event_id:int,payload:EventUpdateSchema)-> EventSchema:
     # a bunch of items in a table
-    print(payload)
+    print(payload.description)
     print(type(payload))
-    return {
-        "results": [{'id':1},{'id':2}] ,
-        "count": 2
-    }
+    data=payload.model_dump() # payload -> dict -> pydantic
+    return {'id':event_id,**data}
 
 # @router.delete("/{event_id}")
 # def delete_events(event_id:int,payload:dict={})-> EventlistSchema:
@@ -50,14 +49,6 @@ def update_events(event_id:int,payload:EventUpdateSchema)-> EventlistSchema:
 #     }
 
 
-# get /spi/events/id
-@router.get("/{event_id}")
-def get_events(event_id:int)->EventSchema:
-
-    # a single row
-    return {
-        "id": event_id
-    }
 
 
 
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,16 +1,21 @@
 from pydantic import BaseModel, Field
+from typing import List, Optional
 from typing import List
 
 
 class EventSchema(BaseModel):
     id: int
+    page: Optional[str]=''
+    description: Optional[str]=''
 
 
 class EventCreateSchema(BaseModel):
-    path: str
+    page: str
+    description: Optional[str]=Field(default="my description")
 
 class EventUpdateSchema(BaseModel):
     description: str
+    page: Optional[str]=''
 
 
 class EventlistSchema(BaseModel):
