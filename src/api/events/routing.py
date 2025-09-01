e3ac945e1c355505af394c5f02b81fe25883ceff
Ebrahim Emad <ebemad8@gmail.com>
2025-09-01 11:30:15 +0300

    update structure

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,8 +1,13 @@
 # api/events/routing.py
 from fastapi import APIRouter
-
+from .schemas import EventSchema,EventListSchema
 router = APIRouter()
 
 @router.get("/")
-def read_events():
-    return {"items": [1, 2, 3]}
+def read_events()->EventListSchema:
+    return {"items": [{'id':1},{'id': 2},{'id': 3}]}
+
+
+@router.get("/{event_id}")
+def get_event(event_id:int)->EventSchema:
+    return {"id":event_id}
