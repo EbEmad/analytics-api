bf0f33bdc383349943a314c8f1a932e1abb0ada6
Ebrahim Emad <ebemad8@gmail.com>
2025-09-02 14:12:17 +0300

    add new schemas

--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,8 +1,21 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel,Field
+from typing import List,Optional
+from sqlmodel import SQLModel,Field
+
+
+class EventCreateSchema(BaseModel):
+    page: str
+    description: Optional[str]=Field(default="")
+
+
+class EventUpdateSchema(BaseModel):
+    description: str
+
 
 class EventSchema(BaseModel):
     id: int
+    page:Optional[str]=""
+    description: Optional[str]=""
 
 
 
