83ab7fd1fb17f462f171dccdc5269a66c162425f
Ebrahim Emad <ebemad8@gmail.com>
2025-09-09 11:16:00 +0300

    adding new features

--- src/api/events/models.py
+++ src/api/events/models.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel,Field
 from typing import List,Optional
 from sqlmodel import SQLModel,Field
+from datetime import datetime,timezone
+from zoneinfo import ZoneInfo
 
-
+def get_date():
+    datetime.now(ZoneInfo("Africa/Cairo"))
 class EventCreateSchema(SQLModel):
     page: str
     description: Optional[str]=Field(default="")
@@ -16,6 +19,10 @@ class EventModel(SQLModel,table=True):
     id: Optional[int]=Field(default=None,primary_key=True)
     page:Optional[str]=""
     description: Optional[str]=""
+    created_at: datetime=Field(
+        default_factory=get_date,
+        sa_type=SQLModel.DateTime(timezone=True),
+    )
 
 
 
--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -36,17 +36,28 @@ def read_events(session:Session=Depends(get_session)):
 
 
 
-@router.get("/{event_id}")
-def get_event(event_id:int)->EventModel:
-    return {"id":event_id}
+@router.get("/{event_id}",response_model=EventModel)
+def get_event(event_id:int,session:Session=Depends(get_session))->EventModel:
+    query=select(EventModel).where(EventModel.id==event_id)
+    result=session.exec(query).first()
+    return result
 
 
 
-@router.put("/{event_id}")
-def update_event(event_id:int,payload:EventUpdateSchema)->EventModel:
+@router.put("/{event_id}",response_model=EventModel)
+def update_event(event_id:int,payload:EventUpdateSchema,session:Session=Depends(get_session)):
+    query=select(EventModel).where(EventModel.id==event_id)
+    obj=session.exec(query).first()
+    if not obj:
+        raise HTTPException(status_code=404,detail="Event not found")
     data=payload.model_dump()
-    return {"id":event_id,**data}
+    for k,v in data.items():
+        setattr(obj,k,v)
 
+    session.add(obj)
+    session.commit()
+    session.refresh(obj)
+    return obj
 
 # @router.delete("/{event_id}")
 # def update_event(event_id:int,payload:dict={})->EventModel:
